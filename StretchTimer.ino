813922e0530da0447b82e7c29ce6fa868504dd3d
kennethbchen <kennethbchen@gmail.com>
2021-02-28 21:00:32 -0500

    Implemented ability to measure time since last stretch break

--- StretchTimer.ino
+++ StretchTimer.ino
@@ -1,85 +1,126 @@
-// the setup function runs once when you press reset or power the board
+// ----- PINS -----
+const int redPin = 12;
+const int greenPin = 11;
+const int bluePin = 10;
 
-int redPin= 12;
-int greenPin = 11;
-int bluePin = 10;
+const int powerButtonPin = 5;
+const int stretchButtonPin = 6;
 
-int buttonPin = 5;
-
-int sitThreshold = 12; // Inches
+// ----------------
+// ---- STATE -----
 
 bool enabled = false;
 bool buttonPressed = false;
+bool shouldStretch = false;
+
+// Seconds
+float lastStretchTime = 0;
+
+// ----------------
+// --- SETTINGS ---
+// Seconds, Time between stretch breaks
+const float stretchInterval = 10;
+
+// Seconds, Duration of stretch break
+const 
+float stretchDuration = 30;
+
+// ----------------
+
 
 void setup() {
   Serial.begin(9600);
-  
+
+  // Setup pins
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
 
-  pinMode(buttonPin, INPUT);
+  pinMode(powerButtonPin, INPUT_PULLUP);
+  pinMode(stretchButtonPin, INPUT_PULLUP);
+
+  // Set last stretch time
+  lastStretchTime = millis();
 }
 
-// the loop function runs over and over again forever
 void loop() {
 
-  if(digitalRead(buttonPin) == 1 && !buttonPressed) {
+  // Detect if the power button is being pressed
+  if (digitalRead(powerButtonPin) == 1 && !buttonPressed) {
+
+    // Used to make each whole button press only excecute once
     buttonPressed = true;
 
-    if(enabled) {
+    // Toggle on / off status
+    if (enabled) {
       enabled = false;
-      RGB_color(0,0,0);
     } else {
       enabled = true;
+      
+      // Reset Stretch Time
+      lastStretchTime = getTimeInSeconds(millis());
     }
-    
-    Serial.print(digitalRead(buttonPin));
+
+    Serial.print(digitalRead(powerButtonPin));
+    Serial.print(lastStretchTime);
     Serial.println();
   }
 
-  if(digitalRead(buttonPin) == 0 && buttonPressed) {
+  // Reset the power button pressed state
+  if (digitalRead(powerButtonPin) == 0 && buttonPressed) {
     buttonPressed = false;
   }
 
-  
-  
-
+  // If the timer is on, then keep track
   if (enabled) {
-    RGB_color(255, 255, 255);
+
+    // If the stretch button pin is pressed
+    if (digitalRead(stretchButtonPin)) {
+
+      // Reset last stretch time to current time
+      lastStretchTime = getTimeInSeconds(millis());
+    }
+    
+    // Interpolate led color based on time till next stretch break
+    writeLED();
+
+    // If it is time for a stretch break
+    if (getTimeInSeconds(millis()) - lastStretchTime > stretchInterval) {
+      shouldStretch = true;
+    }
+
+    if (shouldStretch) {
+      
+    }
+    
   } else {
-    RGB_color(0, 0, 0);
-
+    // If timer is off, turn LED off
+    setLED(0, 0, 0);
   }
-  
 
-  
+
+
 }
 
-void RGB_color(int red_light_value, int green_light_value, int blue_light_value) {
+float getTimeInSeconds(float milliseconds) {
+  return milliseconds / 1000;
+}
+
+void setLED(int red_light_value, int green_light_value, int blue_light_value) {
   analogWrite(redPin, red_light_value);
   analogWrite(greenPin, green_light_value);
   analogWrite(bluePin, blue_light_value);
 }
 
-long microsecondsToInches(long microseconds) {
-   return microseconds / 74 / 2;
-}
+void writeLED() {
 
-void writeLED(float inches) {
+  // Calculate value between 0.0 and 1.0 that indicates percentage of stretch interval passed
+  float proportionLeft = min((float) (getTimeInSeconds(millis()) - lastStretchTime) / stretchInterval, 1);
 
-  
-  if(inches <= sitThreshold) {
-    
-    // If sitting
-    // Green
-    RGB_color(0, 255, 0);
-    
-  } else {
-    
-    // If not sitting
-    // Yellow
-    RGB_color(255, 125, 0);
-    
+  if ( proportionLeft < 1.0 ) {
+    Serial.println(proportionLeft);
+    setLED((int) (255 * (proportionLeft)), (int) (255 * (1 - proportionLeft)), 0);
   }
+
+
 }
