495d3ae9106436a45f1cfab0c397b5128655171a
kennethbchen <kennethbchen@gmail.com>
2023-12-19 15:16:53 -0500

    Started implementing using different distance sensor

--- StretchTimer.ino
+++ StretchTimer.ino
@@ -1,3 +1,13 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+// 7 Segment LED Display
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
+
+// Distance Sensor
+#include <vl53l4cd_class.h>
+
 //https://gitlab.com/yesbotics/libs/arduino/average-value
 #include <AverageValue.h>
 
@@ -23,7 +33,7 @@ const bool snoozeButtonPressedValue = false;
 // --- SETTINGS ---
 
 // Don't leave on for actual use, it slows down arduino or something
-#define PRINT_OUTPUT false
+#define PRINT_OUTPUT true
 
 // Debug mode shortens stretch times and durations
 #define DEBUG_MODE false
@@ -45,6 +55,8 @@ const float motorCycleTime = 1.12;
 // Value from 0 to 1
 // Brightness multiplier for LED
 const float globalBrightnessMultiplier = 0.2;
+
+
 
 // ----------------
 // ---- STATE -----
@@ -78,7 +90,13 @@ float motorActionTime = -1;
 // Running average of IR measurements
 // Used to reduce noise in IR input
 // Slows down significantly if printing to serial
-AverageValue<double> dist(100);
+AverageValue<double> dist_inches(1);
+
+#define DEV_I2C Wire
+
+VL53L4CD distanceSensor = VL53L4CD(&DEV_I2C, A1);
+
+Adafruit_7segment matrix = Adafruit_7segment();
 
 // ----------------
 // ----- UTIL -----
@@ -91,66 +109,85 @@ const int blue[] {0, 0, 255};
 // ----------------
 
 void setup() {
-  
-  Serial.begin(9600);
 
+  #if PRINT_OUTPUT
+    Serial.begin(115200);
+    Serial.println("Starting...");
+  #endif
+  
+  
   // Setup pins
+  /*
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
 
   pinMode(motorPin, OUTPUT);
   pinMode(snoozeButtonPin, INPUT_PULLUP);
+  */
+  
+
+  // Initialize I2C bus.
+  DEV_I2C.begin();
+
+  // Configure VL53L4CD satellite component.
+  distanceSensor.begin();
+
+  // Switch off VL53L4CD satellite component.
+  distanceSensor.VL53L4CD_Off();
+
+  //Initialize VL53L4CD satellite component.
+  distanceSensor.InitSensor();
+
+  // Program the highest possible TimingBudget, without enabling the
+  // low power mode. This should give the best accuracy
+  distanceSensor.VL53L4CD_SetRangeTiming(200, 0);
+
+  // Start Measurements
+  distanceSensor.VL53L4CD_StartRanging();
+
+  // Init Screen
+  matrix.begin(0x70);
 
   lastStretchTime = getTimeInSeconds(millis());
 }
 
-void setMotorOff() {
-  // Motor off
-  motorActionTime = -1;
-  digitalWrite(motorPin, LOW);
-}
-
-void changeToSitting() {
-  currentState = SITTING;
-
-  setMotorOff();
-  
-  // If needed, modify lastStretchTime to ignore time paused 
-  if (timePaused != 0) {
-    lastStretchTime = getTimeInSeconds(millis()) - (timePaused - lastStretchTime);
-    timePaused = 0;
-  }
-}
-
-void changeToPaused() {
-  currentState = PAUSED;
-  timePaused = getTimeInSeconds(millis());
-}
-
-void changeToReminding() {
-  currentState = REMINDING;
-}
-
-void changeToStretching() {
-  currentState = STRETCHING;
-
-  setMotorOff();
-
-  // Record stretch start time
-  stretchStart = getTimeInSeconds(millis());
-}
 
 void loop() {
   
-  // Get distance of the IR sensor
-  dist.push(analogRead(irInput));
+  
+  uint8_t NewDataReady = 0;
+  VL53L4CD_Result_t results;
+  uint8_t status;
+
+  // Wait for distance sensor
+  do {
+    status = distanceSensor.VL53L4CD_CheckForDataReady(&NewDataReady);
+  } while (!NewDataReady);
+
+  
+
+  if ((!status) && (NewDataReady != 0)) {
+    // (Mandatory) Clear HW interrupt to restart measurements
+    distanceSensor.VL53L4CD_ClearInterrupt();
+
+    // Read measured distance. RangeStatus = 0 means valid data
+    distanceSensor.VL53L4CD_GetResult(&results);
+    
+    // Get distance of the sensor
+    dist_inches.push(results.distance_mm / 25.4);
+
+    matrix.print(dist_inches.average());
+    matrix.writeDisplay();
+  }
+  
+  return;
   
   #if PRINT_OUTPUT
     Serial.print("Measurement ");
-    Serial.print(String(dist.average()) + " | ");
+    Serial.print(String(dist_inches.average()) + " | ");
   #endif
-
+  
   // ----------------
   // ---- PAUSED ----
   if (currentState == PAUSED) {
@@ -166,7 +203,6 @@ void loop() {
   // ----------------
   // ---- SITTING ---
   if (currentState == SITTING) {
-  
     // Interpolate led color based on time till next stretch break
     interpolateLED(green, 
                    red, 
@@ -274,12 +310,51 @@ void loop() {
     
 }
 
+#pragma region STATE_METHODS
+
+void changeToSitting() {
+  currentState = SITTING;
+
+  setMotorOff();
+  
+  // If needed, modify lastStretchTime to ignore time paused 
+  if (timePaused != 0) {
+    lastStretchTime = getTimeInSeconds(millis()) - (timePaused - lastStretchTime);
+    timePaused = 0;
+  }
+}
+
+void changeToPaused() {
+  currentState = PAUSED;
+  timePaused = getTimeInSeconds(millis());
+}
+
+void changeToReminding() {
+  currentState = REMINDING;
+}
+
+void changeToStretching() {
+  currentState = STRETCHING;
+
+  setMotorOff();
+
+  // Record stretch start time
+  stretchStart = getTimeInSeconds(millis());
+}
+
+#pragma endregion STATE_METHODS
+
 // ----------------------
 // --- UTIL FUNCTIONS ---
 
+void setMotorOff() {
+  // Motor off
+  motorActionTime = -1;
+  digitalWrite(motorPin, LOW);
+}
+
 bool isSitting() {
-  // dist.average should be within 0-1023 or something (based on output of analog read)
-  return dist.average() > 500 ;
+  return dist_inches.average() > 10 ;
 }
 
 float getTimeInSeconds(float milliseconds) {
@@ -287,6 +362,8 @@ float getTimeInSeconds(float milliseconds) {
 }
 
 void setLED(int red_light_value, int green_light_value, int blue_light_value) {
+
+  return;
   analogWrite(redPin, red_light_value * globalBrightnessMultiplier);
   analogWrite(greenPin, green_light_value * globalBrightnessMultiplier);
   analogWrite(bluePin, blue_light_value * globalBrightnessMultiplier);
